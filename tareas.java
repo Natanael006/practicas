3255306508e3521b733695c6269eb9cfc9a675d1
Natanael006 <143622953+Natanael006@users.noreply.github.com>
2023-09-06 08:48:51 -0600

    Add files via upload

---
+++ tareas.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author Usuario
+ */
+public class tareas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int edad;
+        String tarea1 = "lavar los trastes";
+        String tarea2 = "tender la cama";
+        String tarea3 = "hacer la comida";
+        String tarea4 = "lavar la ropa";
+        String tarea5 = "trapear";
+        final int anionNacimiento = 2000;      
+              
+        String cadenaTexto = "cadena de texto %% $$";
+        
+        boolean prendeluz = true;
+        boolean apagarluz = false;
+        
+       
+                
+        System.out.println("lista de tareas :"+tarea1 +","+tarea2 + ","+tarea3 +"," +tarea4 +"," +tarea5 );
+
+    }
+    
+}
